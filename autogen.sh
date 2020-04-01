ea0500e1bc844c1332d42077d944702b6c971133
turlodales <paulturley_2@hotmail.com>
2020-04-01 19:04:44 +0100

    Update autogen.sh
    updated from s0uthwest version

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
 gprefix=`which glibtoolize 2>&1 >/dev/null`
 if [ $? -eq 0 ]; then
   glibtoolize --force
 else
   libtoolize --force
 fi
-
 aclocal -I m4
-autoconf
 autoheader
 automake --add-missing
+autoconf
 
-if [ -z $NOCONFIGURE ]; then
-  ./configure "$@"
+if [ -z "$NOCONFIGURE" ]; then
+    ./configure "$@"
 fi
